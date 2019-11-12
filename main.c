6ff606177e609e9f180101fc7af9a4e360db380b
gaheunjem <gaheunjem@sookmyung.ac.kr>
2019-11-12 15:45:29 +0900

    실습3

--- main.c
+++ main.c
@@ -5,23 +5,18 @@
 
 int main(int argc, char *argv[]) {
 	
-	char *pc;
-	int *pi;
-	double *pd;
-	
-	pc=(char*)10000;
-	pi=(int*)10000;
-	pd=(double*)10000;
-	
-	printf("증가전 : pc=%d, pi=%d, pd=%d\n",pc,pi,pd);
-	
-	pc++;
-	pi++;
-	pd++;
-	
-	printf("증가후 : pc=%d, pi=%d, pd=%d\n",pc,pi,pd);
-	
-	
+ 	char buffer[8];
+ 	double *pd;
+ 	int *pi;
+ 	
+ 	pd=(double*)buffer;
+ 	*pd=3.14;
+ 	printf("%f\n",*(double*)buffer);
+ 	
+ 	pi=(int*)buffer;
+ 	*pi=123;
+ 	*(pi+1)=456;
+ 	printf("%d%d\n",*(int*)buffer,*((int*)buffer+1));
 	
 	
 	
